bfdb4239dfe444cb29bdb3b91b9b1232c6496a94
JC Naty <monnatyz@gmail.com>
2021-10-14 00:27:56 +0700

    setup layout ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
 
-    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>{{ config('app.name', 'CakeApp') }}</title>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-        <title>CasyCake</title>
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="homeui/assets/css/bootstrap.min.css">
 
-        <!-- Additional CSS Files -->
-        <link rel="stylesheet" type="text/css" href="homeui/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="homeui/assets/css/font-awesome.css">
 
-        <link rel="stylesheet" type="text/css" href="homeui/assets/css/font-awesome.css">
+    <link rel="stylesheet" href="homeui/assets/css/templatemo-klassy-cafe.css">
 
-        <link rel="stylesheet" href="homeui/assets/css/templatemo-klassy-cafe.css">
+    <link rel="stylesheet" href="homeui/assets/css/owl-carousel.css">
 
-        <link rel="stylesheet" href="homeui/assets/css/owl-carousel.css">
+    <link rel="stylesheet" href="homeui/assets/css/lightbox.css">
 
-        <link rel="stylesheet" href="homeui/assets/css/lightbox.css">
+    <!--Custom Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- Custom Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    </head>
+</head>
     
-    <body>
-    
+<body>
+
     <!-- ***** Preloader Start ***** -->
     <div id="preloader">
         <div class="jumper">
@@ -46,28 +49,8 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <nav class="main-nav">
-                        
-                        <!-- ***** Logo Start ***** -->
-                        <a href="/" class="logo">
-                            <!-- <img src="homeui/assets/images/klassy-logo.png" align="klassy cafe html template"> -->
-                        </a>
-                        <!-- ***** Logo End ***** -->
-
-                        <!-- ***** Menu Start ***** -->
-                        <ul class="nav">
-                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                            <li class="scroll-to-section"><a href="#about">About</a></li>
-                            <li class="scroll-to-section"><a href="#menu">Menu</a></li>
-                            <li class="scroll-to-section"><a href="#reservation">Contact Us</a></li> 
-                        </ul> 
-                        <a class='menu-trigger'>
-                            <span>Menu</span>
-                        </a>
-                        <!-- ***** Menu End ***** -->
-                    </nav>
+                    @include('layouts.navigation')
                 </div>
-
             </div>
         </div>
     </header>
@@ -693,5 +676,5 @@
         });
 
     </script>
-  </body>
+</body>
 </html>
