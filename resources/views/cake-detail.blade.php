ac34d73ae8068de4f950b96bf01a231169d73950
Ho Trung Nhan <hotrungnhan2000@gmail.com>
2021-10-17 15:41:05 +0700

    Finish FE Cake-Detail

--- resources/views/cake-detail.blade.php
+++ resources/views/cake-detail.blade.php
@@ -9,24 +9,52 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="grid grid-cols-2 gap-x-3">
-                    <div class="w-96 h-96 p-4 mx-auto">
-                        <img id="mainImg" class="w-full h-full mb-3 rounded-lg object-fill" src="http://4.bp.blogspot.com/-sbpJrKlHCXA/VOmV6vxWpXI/AAAAAAAAAQY/7jPzIfW5HA0/s1600/4.jpg" alt="">
-                        <div class="grid grid-cols-4 gap-x-3">
-                            <div class="w-35 h-35 p-1 mx-auto ">
-                                <img class="sub-img w-full h-full rounded-lg object-fill border-2" src="http://4.bp.blogspot.com/-sbpJrKlHCXA/VOmV6vxWpXI/AAAAAAAAAQY/7jPzIfW5HA0/s1600/4.jpg" alt="">
-                            </div>
-                            <div class="w-35 h-35 p-1 mx-auto ">
-                                <img class="sub-img w-full h-full rounded-lg object-fill border-2" src="http://image.optcdn.me/744b17cdad41f1bf8042ca091bef4316-moist-chocolate-cupcakes-5.jpg" alt="">
-                            </div>
-                            <div class="w-35 h-35 p-1 mx-auto ">
-                                <img class="sub-img w-full h-full rounded-lg object-fill border-2" src="https://cdn.tgdd.vn/2021/07/CookRecipe/GalleryStep/thanh-pham-1049.jpg" alt="">
-                            </div>
-                            <div class="w-35 h-35 p-1 mx-auto ">
-                                <img class="sub-img w-full h-full rounded-lg object-fill border-2" src="https://www.cookingclassy.com/wp-content/uploads/2014/01/cherry-almond-cupcakes-edit+srgb+text..jpg" alt="">
+                    <div class="w-full h-auto p-4">
+                        <!-- <div id="mainImg" class="w-96 h-96 mb-3 mx-auto rounded-lg"></div> -->
+
+                        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="w-96 h-96 mb-3 mx-auto rounded-lg object-fill" src="http://4.bp.blogspot.com/-sbpJrKlHCXA/VOmV6vxWpXI/AAAAAAAAAQY/7jPzIfW5HA0/s1600/4.jpg" alt="">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h4 class="text-semibold text-white">Cupcake Strawberry</h4>
+                                        
+                                    </div>
+                                </div>  
+                                <div class="carousel-item">
+                                    <img class="w-96 h-96 mb-3 mx-auto rounded-lg object-fill" src="http://image.optcdn.me/744b17cdad41f1bf8042ca091bef4316-moist-chocolate-cupcakes-5.jpg" alt="">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h4 class="text-semibold text-white">Cupcake Chocolate</h4>
+                                       
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="w-96 h-96 mb-3 mx-auto rounded-lg object-fill" src="https://cdn.tgdd.vn/2021/07/CookRecipe/GalleryStep/thanh-pham-1049.jpg" alt="">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h4 class="text-semibold text-white">Cupcake Matcha</h4>
+                                       
+                                    </div>
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="w-96 h-96 mb-3 mx-auto rounded-lg object-fill" src="https://www.cookingclassy.com/wp-content/uploads/2014/01/cherry-almond-cupcakes-edit+srgb+text..jpg" alt="">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h4 class="text-semibold text-white">Cupcake Cherry</h4>
+                                       
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
+                       <!--Other Image-->
+                        <div class="w-full h-32 grid grid-cols-4 gap-x-1">
+                            <img class="sub-img w-full h-full mb-3 mx-auto overflow-auto rounded-lg object-cover border-2" src="http://4.bp.blogspot.com/-sbpJrKlHCXA/VOmV6vxWpXI/AAAAAAAAAQY/7jPzIfW5HA0/s1600/4.jpg" alt="">
+                            <img class="sub-img w-full h-full mb-3 mx-auto overflow-auto rounded-lg object-cover border-2" src="http://image.optcdn.me/744b17cdad41f1bf8042ca091bef4316-moist-chocolate-cupcakes-5.jpg" alt="">
+                            <img class="sub-img w-full h-full mb-3 mx-auto overflow-auto rounded-lg object-cover border-2" src="https://cdn.tgdd.vn/2021/07/CookRecipe/GalleryStep/thanh-pham-1049.jpg" alt="">
+                            <img class="sub-img w-full h-full mb-3 mx-auto overflow-auto rounded-lg object-cover border-2" src="https://www.cookingclassy.com/wp-content/uploads/2014/01/cherry-almond-cupcakes-edit+srgb+text..jpg" alt="">
+                        </div>
+                        <!--Other Imange end-->
                     </div>
-                    <div>
+                    <div class="w-full h-auto p-4">
                         <h4 class="font-semibold text-3xl text-black py-6">Bánh Cupcake</h4>
                         <span class="block font-normal text-xl text-red-400">35.000Đ</span>
                         <span class="block py-6 mr-4 font-light text-base text-gray-400">Cupcake đã được 200 tuổi rồi đó. 
@@ -44,8 +72,6 @@
                             <button class="inline-lock px-6 py-1 m-1 text-gray-700 focus:bg-gray-300 rounded-lg border-2" >
                                 Chocolate
                             </button>
-                            <!--Image chocolate  http://image.optcdn.me/744b17cdad41f1bf8042ca091bef4316-moist-chocolate-cupcakes-5.jpg -->
-                            <!-- Image trà xanh https://cdn.tgdd.vn/2021/07/CookRecipe/GalleryStep/thanh-pham-1049.jpg -->
                             <button class="inline-lock px-6 py-1 m-1 text-gray-700 focus:bg-gray-300 rounded-lg border-2" >
                                 Trà xanh
                             </button>
@@ -57,7 +83,7 @@
                         <input class="w-20 h-10 mx-3 rounded-lg" type="number" min="0">
                         <button class="block px-6 py-3 m-3 text-white bg-red-400 hover:bg-red-500 rounded-lg transition-colors">Thêm vào giỏ hàng</button>
                         <hr>
-                        <span class="block py-4 font-light text-sm text-gray-400">Shop đảm bảo giao hàng không quá 24 giờ, miễn phí trả hàng nếu không đúng yêu cầu</span>
+                        <span class="block pt-4 font-light text-sm text-gray-400">Shop đảm bảo giao hàng không quá 24 giờ, miễn phí trả hàng nếu không đúng yêu cầu</span>
                     </div>
                 </div>
             </div>
