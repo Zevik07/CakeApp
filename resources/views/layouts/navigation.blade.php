7ae53ad74259d92e0c58ade37f01908990508a61
JC Naty <monnatyz@gmail.com>
2021-10-19 23:57:25 +0700

    update navbar main page ui

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="./">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <x-application-logo class="block h-14 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
 
@@ -23,9 +23,6 @@
                     </x-nav-link>
 
                 </div>
-            </div>
-            <div class="flex items-center">
-                Giò hàng
             </div>
             @auth
             <!-- Settings Dropdown -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -131,25 +131,27 @@
             <div class="col-lg-12">
                 <div class="owl-menu-item owl-carousel owl-theme">
                     <div class="item slide" data-slide-index="0">
-                        <div class='card card1'>
-                            <div class="price"><h6>$14</h6></div>
-                            <div class='info'>
-                                <h1 class='title'>Chocolate Cake</h1>
-                                <p class='description'>
-                                Đây là một loại bánh1
-                                Đây là một loại bánh
-                                Đây là một loại bánh
-                                Đây là một loại bánh
-                            </p>
-                                <div class="main-text-button">
-                                    <div class="scroll-to-section">
-                                        <a href="#reservation">
-                                            Thêm vào giỏ bánh
-                                            <i class="fas fa-shopping-cart"></i> 
-                                        </a>
+                        <div href="#" class='card card1'>
+                            <a href="#">
+                                <div class="price"><h6>$14</h6></div>
+                                <div class='info'>
+                                    <h1 class='title'>Chocolate Cake</h1>
+                                    <p class='description'>
+                                    Đây là một loại bánh1
+                                    Đây là một loại bánh
+                                    Đây là một loại bánh
+                                    Đây là một loại bánh
+                                    </p>
+                                    <div class="main-text-button">
+                                        <div class="scroll-to-section">
+                                            <a href="#reservation">
+                                                Thêm vào giỏ bánh
+                                                <i class="fas fa-shopping-cart"></i> 
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </a>
                         </div>
                     </div>
                     <div class="item slide" data-slide-index="1">
@@ -414,14 +416,11 @@
                             CasyCake được thành lập vào 2021
                         </p>
                         <div class="row">
-                            <div class="col-4">
+                            <div class="col-6">
                                 <img src="/homeui/assets/images/menu-item-01.jpg" alt="">
                             </div>
-                            <div class="col-4">
+                            <div class="col-6">
                                 <img src="/homeui/assets/images/menu-item-02.jpg" alt="">
-                            </div>
-                            <div class="col-4">
-                                <img src="/homeui/assets/images/menu-item-03.jpg" alt="">
                             </div>
                         </div>
                     </div>
