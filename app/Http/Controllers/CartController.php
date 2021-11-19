77a9f4360d7f8ed3dc79712a94368bcdfcb8908a
JC Naty <monnatyz@gmail.com>
2021-11-19 14:28:08 +0700

    fix cart validate

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,23 +9,10 @@ use Illuminate\Support\Facades\Session;
 
 class CartController extends Controller
 {   
-    
-    // function order(Request $req){
-    //     $data=$req->input();
-    //     Session::put('username',$data['username']);
-    //     Session::put('address',$data['address']);
-    //     Session::put('phone',$data['phone']);
-    //     Session::put('email',$data['email']);
-    //     $req->session()->flash('username', $data['username']);
-    //     $req->session()->flash('id', $data['address']);
-    //     $req->session()->flash('phone', $data['phone']);
-    //     $req->session()->flash('email', $data['email']);
-    //     return view('order');
-        
-    // }
     function index(){
         return view('cart');
     }
+
     public function detele(Request $request){
         $id = $request['id'];
         $cart =$request->session()->get('session_cart'); 
@@ -41,7 +28,6 @@ class CartController extends Controller
             }
             
         $request->Session()->put('session_cart', $cart);
-          
         return back()->withInput();
-         }
+    }
 }
